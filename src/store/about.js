c627e6040b7328eff1589d941c2b089c41c967a7
Zueon <maineon0701@gmail.com>
2022-04-05 22:27:04 +0900

    edit profile

--- src/store/about.js
+++ src/store/about.js
@@ -5,7 +5,6 @@ export default {
     email: 'maineon0701@gmail.com',
     blog: 'https://velog.io/@zueon',
     phone: '010-1234-1234',
-    image:
-      'https://media.vlpt.us/images/zueon/profile/8975b13b-c517-40bc-9247-a0edf075eab6/social.jpeg?w=240',
+    image: 'blob:https://velog.io/381b75f2-bc49-45b5-a713-ff2eaa045e3b',
   }),
 };
